2c63f94f123cd4e6c01647a1aec7e2e2e61d846f
Hai Phong <teothiphong@email.com>
2022-05-10 20:26:47 +0700

    edit file config

--- src/main/java/com/group4/group4/config/WebSecurityConfig.java
+++ src/main/java/com/group4/group4/config/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
-                .anyRequest().authenticated();
+        ;
 
         http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
