b02cb942e87bc267776d34f482bcca7f29294a85
Hai Phong <teothiphong@email.com>
2022-05-10 23:00:40 +0700

    edit request object

--- src/main/java/com/group4/group4/payload/request/LoginRequest.java
+++ src/main/java/com/group4/group4/payload/request/LoginRequest.java
@@ -1,9 +1,13 @@
 package com.group4.group4.payload.request;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;
--- src/main/java/com/group4/group4/payload/request/RegisterRequest.java
+++ src/main/java/com/group4/group4/payload/request/RegisterRequest.java
@@ -1,8 +1,12 @@
 package com.group4.group4.payload.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
 @Data
 @AllArgsConstructor
 public class RegisterRequest {
