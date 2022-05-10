462f609ebdb20f5d8315f0f51d4a71b1d3e1352d
Hai Phong <teothiphong@email.com>
2022-05-10 16:14:40 +0700

    create method login

--- src/main/java/com/group4/group4/controller/HomeController.java
+++ src/main/java/com/group4/group4/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.group4.group4.controller;
 import com.group4.group4.jwt.JwtTokenProvider;
 import com.group4.group4.payload.request.LoginRequest;
 import com.group4.group4.payload.response.LoginResponse;
+import com.group4.group4.repository.IRoleRepository;
 import com.group4.group4.repository.IUserRepository;
 import com.group4.group4.service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
+@RequestMapping("/api/auth")
 public class HomeController {
     @Autowired
     AuthenticationManager authenticationManager;
@@ -30,8 +34,6 @@ public class HomeController {
     PasswordEncoder passwordEncoder;
     @Autowired
     IRoleRepository iRoleRepository;
-    @Autowired
-    RefreshTokenService refreshTokenService;
 
     @PostMapping("/login")
     public LoginResponse getResponseAfterLogin(@RequestBody LoginRequest loginRequest){
@@ -43,19 +45,17 @@ public class HomeController {
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        String jwtToken = jwtTokenProvider.generateTokenFormUserName(userDetails.getUsername());
+        String jwtToken = jwtTokenProvider.generateTokenFormEmail(userDetails.getUsername());
         List<String> roles = userDetails.getAuthorities().stream()
                 .map(item -> item.getAuthority())
                 .collect(Collectors.toList());
 
-        RefreshTokenEntity refreshTokenEntity = refreshTokenService.createRefreshToken(userDetails.getUserEntity().getId());
         return LoginResponse.builder()
                 .id(userDetails.getUserEntity().getId())
-                .username(userDetails.getUsername())
+                .email(userDetails.getUsername())
                 .accessToken(jwtToken)
                 .tokenType(new LoginResponse().getTokenType())
                 .role(roles)
-                .refreshToken(refreshTokenEntity.getRefreshToken())
                 .build();
     }
 }
